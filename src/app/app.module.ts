836b38217c2b0b25f8d06ed383c6344d67ac9549
Joe Robinson <josephr1981@live.com>
2022-10-18 17:44:56 -0400

    Initial app module. Has the components of each page and the routing module imported.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { FooterComponent } from './footer/footer.component';
+import { TokyoComponent } from './tokyo/tokyo.component';
+import { KyotoComponent } from './kyoto/kyoto.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    FooterComponent,
+    TokyoComponent,
+    KyotoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
