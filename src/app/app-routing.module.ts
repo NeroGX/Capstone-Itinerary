39fe88b26f39a7228d51411a081c023103dbad19
Joe Robinson <josephr1981@live.com>
2022-10-18 18:15:10 -0400

    Added the routes to each component to allow for navigation on the nav bar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { TokyoComponent } from './tokyo/tokyo.component';
+import { KyotoComponent } from './kyoto/kyoto.component';
 
-
+const routes: Routes = [
+  { path: 'Home', component:AppComponent },
+  { path: 'About Me', component: AboutMeComponent},
+  { path: 'Tokyo', component: TokyoComponent},
+  { path: 'Kyoto', component: KyotoComponent}
+]
 
 @NgModule({
   declarations: [],
